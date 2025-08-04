ab73c97227e0cbdc23169adf7e89da1ea6983374
SurajSushant <surajsushant007@gmail.com>
2025-08-04 15:43:57 +0530

    Reverted change of Instuctions button removed from Test Main Page

--- src/pages/new/TestMainPage.tsx
+++ src/pages/new/TestMainPage.tsx
@@ -531,6 +531,7 @@ const TestMainPage = () => {
             <button className="w-full bg-gray-600 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded-lg">
               Question Paper
             </button>
+            <button className="w-full bg-gray-600 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded-lg">Instructions</button>
             <button
               onClick={handleSubmitTest}
               className="w-full bg-green-600 hover:bg-green-700 text-white font-bold py-3 px-4 rounded-lg"
