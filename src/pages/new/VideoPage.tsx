de3ad1dfb52c3000064c3c3d32cad90f0a13e689
SurajSushant <surajsushant007@gmail.com>
2025-08-08 20:05:00 +0530

    fixed no scroll at AI Tutor component

--- src/pages/new/VideoPage.tsx
+++ src/pages/new/VideoPage.tsx
@@ -290,7 +290,7 @@ const AITutorPanel: React.FC<{
       </div>
 
       {/* Component Container */}
-      <div className="flex-1 overflow-hidden min-h-0">
+      <div className="flex-1 overflow-y-auto min-h-0">
         {components[currentMode]}
       </div>
     </div>
